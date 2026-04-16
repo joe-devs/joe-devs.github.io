62a5d91ba41aba34343ebc88023304bab2ddc832
Joe <goe.89@outlook.com>
2026-04-16 09:39:21 +0200

    edit sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,9 +82,9 @@ const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
 
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
+    const itemCategory = filterItems[i].dataset.category ? filterItems[i].dataset.category.toLowerCase() : "";
+
+    if (selectedValue === "all" || selectedValue === itemCategory) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
